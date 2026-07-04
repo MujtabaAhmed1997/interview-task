25bba7d756822611f224d61c3d9b65f6d4e3050a
Mujtabah Ahmed <iammujtabaahmed@gmail.com>
2026-07-04 13:03:06 +0500

    feat: add sequelize instance with connect/disconnect lifecycle

---
+++ src/common/config/database.ts
@@ -0,0 +1,25 @@
+import { Sequelize } from 'sequelize';
+import { logger } from '../util/logger';
+import { isTest, secrets } from '../util/secrets';
+
+const databaseName = isTest() ? `${secrets.db.name}_test` : secrets.db.name;
+
+export const sequelize = new Sequelize(databaseName, secrets.db.user, secrets.db.password, {
+  host: secrets.db.host,
+  port: secrets.db.port,
+  dialect: 'postgres',
+  logging: false,
+  define: {
+    underscored: true,
+    timestamps: true,
+  },
+});
+
+export const connectDatabase = async (): Promise<void> => {
+  await sequelize.authenticate();
+  logger.info({ event: 'db.connected', database: databaseName });
+};
+
+export const disconnectDatabase = async (): Promise<void> => {
+  await sequelize.close();
+};
